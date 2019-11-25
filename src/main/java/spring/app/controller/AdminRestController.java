e91abfdfd9db1bec36fa3ff81c393b795743584f
Ivan Cherepica <ussr211114@gmail.com>
2019-11-25 13:37:52 +0300

    Добавил рест кнотроллер для админа

---
+++ src/main/java/spring/app/controller/AdminRestController.java
@@ -0,0 +1,9 @@
+package spring.app.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest/admin")
+public class AdminRestController {
+}
