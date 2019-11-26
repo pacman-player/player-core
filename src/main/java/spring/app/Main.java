ef6112690fb60c2bd7a94ced8734f4592ec4e1d3
tagaibek <adyl2692@gmail.com>
2019-11-26 01:45:04 -1100

    удалил лишний импорт файл

--- src/main/java/spring/app/Main.java
+++ src/main/java/spring/app/Main.java
@@ -10,7 +10,6 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import spring.app.configuration.initializator.TestDataInit;
 
-import javax.activation.DataSource;
 import javax.annotation.PostConstruct;
 import java.io.BufferedReader;
 import java.io.FileReader;
