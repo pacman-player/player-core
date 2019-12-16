74816f11f9d20cdcc5cb770ecaf723704d87802a
farrukh13git <farrukh_kk@mail.ru>
2019-12-16 22:29:27 +0500

    Initial commit

--- src/main/java/spring/app/service/impl/SearchServiceImpl.java
+++ src/main/java/spring/app/service/impl/SearchServiceImpl.java
@@ -1,14 +1,21 @@
 package spring.app.service.impl;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import spring.app.service.abstraction.SearchService;
 
+import java.io.IOException;
+
 @Service
+@PropertySource("classpath:uploadedFilesPath.properties")
 public class SearchServiceImpl implements SearchService {
 
-    private final String BASE_URL = "https://api.spotify.com/v1/search?";
+    private final String SEARCH_BASE_URL = "https://downloadmusicvk.ru/audio/search?q=";
+    private final String DOWNLOAD_BASE_URL = "https://downloadmusicvk.ru/audio/play?";
 
     private final RestTemplate restTemplate;
 
@@ -19,5 +26,17 @@ public class SearchServiceImpl implements SearchService {
 
     @Override
     public void search(String artist, String track) {
+        Document document = null;
+        String searchUrl = SEARCH_BASE_URL + artist.trim().toLowerCase().replaceAll("\\s", "+") +
+                "+" + track.trim().toLowerCase().replaceAll("\\s", "+");
+
+        try {
+            document = Jsoup.connect(searchUrl).get();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("Error search on downloadmusicvk.ru");
+        }
+
+
     }
 }
