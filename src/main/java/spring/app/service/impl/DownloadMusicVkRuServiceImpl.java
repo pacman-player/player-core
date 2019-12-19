fcff044e1504f565587935d5dbb903ec0b586b3c
farrukh13git <farrukh_kk@mail.ru>
2019-12-19 09:46:32 +0500

    изменения после замечания

--- src/main/java/spring/app/service/impl/DownloadMusicVkRuServiceImpl.java
+++ src/main/java/spring/app/service/impl/DownloadMusicVkRuServiceImpl.java
@@ -20,6 +20,7 @@ import spring.app.service.abstraction.SongService;
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -49,6 +50,7 @@ public class DownloadMusicVkRuServiceImpl implements DownloadMusicVkRuService {
 
         final String SEARCH_BASE_URL = "https://downloadmusicvk.ru/audio/search?q=";
         boolean success = false;
+        boolean found = false;
 
         if (songService.isExist(track)) {
             return new ResponseEntity<>(encode("Файл с таким названием существует в базе"), HttpStatus.BAD_REQUEST);
@@ -64,11 +66,22 @@ public class DownloadMusicVkRuServiceImpl implements DownloadMusicVkRuService {
 
         Elements aElements = document.getElementsByAttributeValue("class", "btn btn-primary btn-xs download");
         for (Element aElement : aElements) {
+
             String[] arr = aElement.attr("href").substring(19).split("&");
             if (arr.length < 6) {
                 continue;
             }
 
+            if (!decode(arr[1].substring(7)).replaceAll("\\+", " ").toLowerCase().contains(artist.trim().toLowerCase()) ||
+                    !decode(arr[2].substring(6)).replaceAll("\\+", " ").toLowerCase().contains(track.trim().toLowerCase())) {
+                found = false;
+                continue;
+            }
+
+            found=true;
+            if (artist.isEmpty()) {
+                artist = decode(arr[1].substring(7)).replaceAll("\\+", " ").toLowerCase();
+            }
             String downloadUrl = "https://downloadmusicvk.ru/audio/download?" +
                     arr[1] + "&" + arr[2] + "&" + arr[5] + "&" + arr[0];
 
@@ -79,27 +92,7 @@ public class DownloadMusicVkRuServiceImpl implements DownloadMusicVkRuService {
 
             if (responseCode == HttpURLConnection.HTTP_OK) {
                 InputStream in = con.getInputStream();
-
-                Genre genre = genreService.getByName("undefined");
-                if (genre == null) {
-                    genreService.addGenre(new Genre("undefined"));
-                }
-                genre = genreService.getByName("undefined");
-
-                Author author = authorService.getByName(artist);
-                if (author == null) {
-                    Author author1 = new Author(artist);
-                    authorService.addAuthor(author1);
-                    author = authorService.getByName(artist);
-                }
-
-                Song song = new Song(track);
-                song.setAuthor(author);
-                song.setGenre(genre);
-                songService.addSong(song);
-                song = songService.getByName(track);
-
-                String fileName = author.getId() + "_" + song.getId() + ".mp3";
+                String fileName = addToDatabase(artist, track);
                 Path path = Paths.get(fileFolder + fileName);
                 Files.deleteIfExists(path);
                 Files.copy(in, Files.createFile(path), REPLACE_EXISTING);
@@ -109,10 +102,37 @@ public class DownloadMusicVkRuServiceImpl implements DownloadMusicVkRuService {
             }
         }
 
+        if (!found) {
+            return new ResponseEntity<>(encode("Нет совпадений по поиску"), HttpStatus.BAD_REQUEST);
+        }
+
         if (!success) {
             return new ResponseEntity<>(encode("Не возможно загрузить по этому запросу"), HttpStatus.BAD_REQUEST);
         }
         return new ResponseEntity<>(encode("Файл загружен"), HttpStatus.OK);
+    }
+
+    private String addToDatabase(String artist, String track) {
+        Genre genre = genreService.getByName("undefined");
+        if (genre == null) {
+            genreService.addGenre(new Genre("undefined"));
+            genre = genreService.getByName("undefined");
+        }
+
+        Author author = authorService.getByName(artist);
+        if (author == null) {
+            Author author1 = new Author(artist);
+            authorService.addAuthor(author1);
+            author = authorService.getByName(artist);
+        }
+
+        Song song = new Song(track);
+        song.setAuthor(author);
+        song.setGenre(genre);
+        songService.addSong(song);
+        song = songService.getByName(track);
+
+        return author.getId() + "_" + song.getId() + ".mp3";
     }
 
     private String encode(String line) throws UnsupportedEncodingException {
@@ -123,5 +143,9 @@ public class DownloadMusicVkRuServiceImpl implements DownloadMusicVkRuService {
                 .replaceAll("\\%28", "(")
                 .replaceAll("\\%29", ")")
                 .replaceAll("\\%7E", "~");
+    }
+
+    private String decode(String line) throws UnsupportedEncodingException {
+        return URLDecoder.decode(URLDecoder.decode(line, "UTF-8"), "UTF-8");
     }
 }
