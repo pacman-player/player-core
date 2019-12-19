318af8c17d4b6c7e821cc4b87d0bb417e2297151
balmasov <ivanbalmasov@gmail.com>
2019-12-19 22:56:14 +0700

    1. добавлены методы в filterMusic.js для поиска по авторам и песням и генерации таблицы по найденым эллементам 2. в filterMusic добавлены эллементы поиска для разделов меню "По артистам" и "По песням" и div'ы, куда будут вставляться найденные эллементы

--- src/main/resources/static/js/user/filterMusic.js
+++ src/main/resources/static/js/user/filterMusic.js
@@ -30,7 +30,7 @@
                             <button
                                 type="button"
                                 class="btn btn-outline-danger"
-                                onclick="addToFilter(${genreId})"
+                                onclick="addGenreToFilter(${genreId})"
                             >
                                 Заблокировать
                             </button>
@@ -39,31 +39,140 @@
                     `;
             }
             tbl += "</table>";
-            $("#forJsScript").html(tbl);
+            $("#forGenreTable").html(tbl);
         }
     });
 
     // функция для добавления жанра в список запрещеных вызываеться при клике на кнопку
-    function addToFilter(id) {
+    function addGenreToFilter(id) {
         $.ajax({
             method: 'get',
-            url: '/filterForGenre/' + id,
+            url: '/filterForGenre/' + id, //TODO сделать методы
             dataType: 'json',
-            success: function () {}
+            success: function () {
+                //TODO после добавления жанра в бан кнопка напротив этого жанра должна быть помечена
+            }
         });
     }
 
+    // код для поиска и вывода артистов
+    var $authorForms = $(".authorSearch");
+    $authorForms.on("submit", function(searchAuthor){
 
+        searchAuthor.preventDefault();
 
-/*
-tbl += `
-                <tr>
-                    <th>
-                        <form class="form-inline">
-                            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                        </form>
-                    </th>
-                </tr>
-                    `;
- */
+        $.ajax({
+            type: 'get',
+            url: '/api/user/allAuthors',
+            contentType: 'application/json;',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            async: true,
+            cache: false,
+            dataType: 'JSON',
+
+            success: function (allAuthors) {
+                var tbl = "<table class=\"table table-responsive{max-width 768px} table-borderless\">";
+
+                for (var i = 0; i < allAuthors.length; ++i) {
+                    var authorId = allAuthors[i].id;
+                    tbl +=
+                        `
+                        <tr>
+                            <td>
+                                ${allAuthors[i].name}                        
+                            </td> 
+                            <!-- кнопка по добавлению в фильтр автора -->
+                            <td>
+                                <button
+                                    type="button"
+                                    class="btn btn-outline-danger"
+                                    onclick="addAuthorToFilter(${authorId})"
+                                >
+                                    Заблокировать
+                                </button>
+                            </td>
+                        
+                        </tr>
+                        `;
+                }
+                tbl += "</table>";
+                $("#forAuthorTable").html(tbl);
+            }
+        });
+    });
+
+    // функция для добавления исполнителя в список запрещеных вызываеться при клике на кнопку
+    function addAuthorToFilter(id) {
+        $.ajax({
+            method: 'get',
+            url: '/filterForAuthor/' + id, //TODO сделать методы
+            dataType: 'json',
+            success: function () {
+                //TODO после добавления жанра в бан кнопка напротив этого жанра должна быть помечена
+            }
+        });
+    }
+
+    // код для поиска и вывода песен
+    var $musicForm = $(".musicSearch");
+    $musicForm.on("submit", function(searchMusics){
+
+        searchMusics.preventDefault();
+
+        $.ajax({
+            type: 'get',
+            url: '/api/user/allMusic',
+            contentType: 'application/json;',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            async: true,
+            cache: false,
+            dataType: 'JSON',
+
+            success: function (allMusics) {
+                var tbl = "<table class=\"table table-responsive{max-width 768px} table-borderless\">";
+
+                for (var i = 0; i < allMusics.length; ++i) {
+                    var musicId = allMusics[i].id;
+                    tbl +=
+                        `
+                        <tr>
+                            <td>
+                                ${allMusics[i].name}                        
+                            </td> 
+                            <!-- кнопка по добавлению в фильтр автора -->
+                            <td>
+                                <button
+                                    type="button"
+                                    class="btn btn-outline-danger"
+                                    onclick="addMusicToFilter(${musicId})"
+                                >
+                                    Заблокировать
+                                </button>
+                            </td>
+                        
+                        </tr>
+                        `;
+                }
+                tbl += "</table>";
+                $("#forMusicTable").html(tbl);
+            }
+        });
+    });
+
+    // функция для добавления песни(ен) в список запрещеных вызываеться при клике на кнопку
+    function addMusicToFilter(id) {
+        $.ajax({
+            method: 'get',
+            url: '/filterForMusic/' + id, //TODO сделать методы
+            dataType: 'json',
+            success: function () {
+                //TODO после добавления жанра в бан кнопка напротив этого жанра должна быть помечена
+            }
+        });
+    }
