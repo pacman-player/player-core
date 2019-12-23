ebb1c95fa3f3f12bbb29c83b630a03d3df1cf18e
vitandr <asdasdasd#dfsdfsadfsdf>
2019-12-23 17:02:45 +0500

    Добавил сохранение в папку player-core/resources/songs

--- src/main/java/spring/app/util/PlayerPaths.java
+++ src/main/java/spring/app/util/PlayerPaths.java
@@ -1,39 +1,28 @@
 package spring.app.util;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import spring.app.Main;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-@Configuration
-@PropertySource("classpath:uploadedFilesPath.properties")
 public class PlayerPaths {
 
-    @Value("${songs_dir}")
-    private static String songsDir;
-
-    @Value("${disk_letter}")
-    private static String diskLetter;
-
-    @Value("${use_home_dir}")
-    private static boolean useHomeDir;
-
-    public static Path getSongsDir(String filename) throws IOException, URISyntaxException {
-        final String os = System.getProperty("os.name").toLowerCase();
-        final String home = System.getProperty("user.home");
+    public static Path getSongsDir(String filename) {
         final String separator = File.separator;
-        Path path = Paths.get(PlayerPaths.class.getResource(".").toURI());
-        String path1 = path.getParent().getParent().toString() + songsDir + separator + filename;
 
-        return Paths.get(path1);
-    }
-
-    public static void main(String[] args) throws URISyntaxException, IOException {
-        System.out.println(getSongsDir("file.mp3"));
+        try {
+            Path pathProject = (Paths.get(Main.class.getResource(".").toURI())).getParent().getParent();
+            Path pathDownload = Paths.get(pathProject.getParent().getParent().toString() + separator + "resources" + separator + "songs");
+            if (!Files.exists(pathDownload)) {
+                Files.createDirectories(pathDownload);
+            }
+            return Paths.get(pathDownload + separator + filename);
+        } catch (URISyntaxException | IOException ignored) {
+        }
+        return null;
     }
 }
