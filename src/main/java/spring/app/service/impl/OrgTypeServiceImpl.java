7a6929ea2ed76f6a9fddd7d4e5bb22afc948006c
LightDrag-on <lightdragon007@gmail.com>
2019-12-24 13:01:26 +0500

    Исправлена проблема с сериализацией объектов при LAZY. Добавлена страница заведений.

--- src/main/java/spring/app/service/impl/OrgTypeServiceImpl.java
+++ src/main/java/spring/app/service/impl/OrgTypeServiceImpl.java
@@ -6,6 +6,8 @@ import spring.app.dao.abstraction.OrgTypeDao;
 import spring.app.model.OrgType;
 import spring.app.service.abstraction.OrgTypeService;
 
+import java.util.List;
+
 @Service
 public class OrgTypeServiceImpl implements OrgTypeService {
 
@@ -19,5 +21,20 @@ public class OrgTypeServiceImpl implements OrgTypeService {
     @Override
     public void addOrgType(OrgType orgType) {
         orgTypeDao.save(orgType);
+    }
+
+    @Override
+    public void deleteOrgTypeById(Long id) {
+        orgTypeDao.deleteById(id);
+    }
+
+    @Override
+    public void updateOrgType(OrgType orgType) {
+        orgTypeDao.update(orgType);
+    }
+
+    @Override
+    public List<OrgType> getAllOrgType() {
+        return orgTypeDao.getAll();
     }
 }
