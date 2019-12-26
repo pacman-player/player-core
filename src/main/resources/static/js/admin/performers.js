c936ec1cb871d848d25fbd31a0978c7e312c640b
Shell26 <M16VSAK47vsgit>
2019-12-26 12:08:37 +0400

    добавляю таблицу исполнителей

--- src/main/resources/static/js/admin/performers.js
+++ src/main/resources/static/js/admin/performers.js
@@ -3,7 +3,57 @@ $(document).ready(function () {
     getTable();
 
     function getTable(){
+        $.ajax({
+            type: 'get',
+            url: "/api/admin/author/all_author",
+            contentType: 'application/json',
+            headers: {
+                'Accept':'application/json',
+                'Content-Type': 'application/json'
+            },
+            async: true,
+            cache: false,
+            dataType: 'JSON',
+            success: function (listAuthors) {
+                var htmlTable = "";
 
+                for (var i = 0; i < listAuthors.length; i++){
+                    htmlTable += ('<tr id="list">');
+                    htmlTable += ('<td id="authorID">' + listAuthors[i].id + '</td>');
+                    htmlTable += ('<td id="authorName">' + listAuthors[i].name + '</td>');
+                    htmlTable += ('<td><button id="editAuthorBtn" class="btn btn-sm btn-info" type="button" data-toggle="modal" data-target="#editAuthor">Изменить</button></td>');
+                    htmlTable += ('<td><button id="deleteAuthor" class="btn btn-sm btn-info" type="button">Удалить</button> </td>');
+                    htmlTable += ('</tr>');
+                }
+                $("#AuthorTable #list-authors").remove();
+                $("#getAuthorTable").after(htmlTable);
+            }
+        });
+    }
+
+    //add form
+    $('#addAuthorBtn').click(function (event) {
+        event.preventDefault();
+        addAuthor();
+    });
+
+    function addAuthor(){
+        var name = $("#addAuthor").val();
+
+        $.ajax({
+            type: 'post',
+            url: "/api/admin/author/add_author",
+            contentType: 'application/json',
+            data: JSON.stringify(name),
+            headers: {
+                'Accept':'application/json',
+                'Content-Type': 'application/json'
+            },
+            async: false,
+            cache: false,
+            dataType: 'JSON'
+        });
+        location.reload();
     }
 
     //delete form
@@ -15,7 +65,7 @@ $(document).ready(function () {
     function deleteAuthor(id) {
         $.ajax({
             type: 'delete',
-            url: '/api/admin/author/delete_author',
+            url: "/api/admin/author/delete_author",
             contentType: 'application/json',
             data: JSON.stringify(id),
             headers: {
@@ -24,19 +74,37 @@ $(document).ready(function () {
             },
             async: false,
             cache: false,
-            dataType: 'JSON',
+            dataType: 'JSON'
         });
         location.reload();
-    };
-
-    //update form
-    $("#editAuthor").click(function (event) {
-        event.preventDefault();
-        updateAuthor()
-    })
-
-    function updateAuthor() {
-
     }
 
-})
+    //update form
+    $("#editAuthorBtn").click(function (event) {
+        event.preventDefault();
+        updateAuthor()
+    });
+
+    function updateAuthor() {
+        var author = {
+            "id":$("#updateAutorId").val(),
+            "name":$("#updateAutorName").val()
+        };
+
+        $.ajax({
+            type: 'put',
+            url: "/api/admin/author/update_author",
+            contentType: 'application/json',
+            data: JSON.stringify(author),
+            headers:{
+                'Accept':'application/json',
+                'Content-Type': 'application/json'
+            },
+            async: true,
+            cache: false,
+            dataType: 'JSON'
+        });
+        location.reload();
+    }
+
+});
