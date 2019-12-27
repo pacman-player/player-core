c84db6964039ee56946f21f795217ede76748714
shiliubei <shiliubei@gmail.com>
2019-12-27 13:38:01 +0200

    some changes

--- src/main/java/spring/app/service/impl/TelegramServiceImpl.java
+++ src/main/java/spring/app/service/impl/TelegramServiceImpl.java
@@ -1,15 +1,12 @@
 package spring.app.service.impl;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import spring.app.dto.SongRequest;
-import spring.app.dto.SongResponce;
+import spring.app.dto.SongResponse;
 import spring.app.service.abstraction.TelegramService;
 import spring.app.service.abstraction.ZaycevSaitServise;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 @Service
 public class TelegramServiceImpl implements TelegramService {
@@ -21,12 +18,12 @@ public class TelegramServiceImpl implements TelegramService {
     }
 
     @Override
-    public SongResponce getSong(SongRequest songRequest) throws IOException {
+    public SongResponse getSong(SongRequest songRequest) throws IOException {
 
         byte[] bytes = zaycevSaitServise.getSong(songRequest.getAuthorName(),songRequest.getSongName());
-        SongResponce songResponce = new SongResponce(songRequest.getChatId(), 242345367l, bytes,
+        SongResponse songResponse = new SongResponse(songRequest.getChatId(), 242345367l, bytes,
                 songRequest.getAuthorName()+ " - " + songRequest.getSongName());
 
-        return songResponce;
+        return songResponse;
     }
 }
