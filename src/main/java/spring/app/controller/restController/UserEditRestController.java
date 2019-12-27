cf75cd53a7d3e409184446c5aa97a0868855ab29
Shell26 <M16VSAK47vsgit>
2019-12-27 13:27:51 +0400

    добавил UserEditRestController делаю js и html страницы

---
+++ src/main/java/spring/app/controller/restController/UserEditRestController.java
@@ -0,0 +1,37 @@
+package spring.app.controller.restController;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import spring.app.model.User;
+import spring.app.service.abstraction.UserService;
+
+
+@RestController
+@RequestMapping(value = "/api/user/edit/")
+public class UserEditRestController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserEditRestController(UserService userService) {
+        this.userService = userService;
+    }
+
+//    @GetMapping(value = "/get_user")
+//    public User getUserData(@RequestBody User user){
+//        return userService.getUserById(id);
+//    }
+
+    @PutMapping(value = "/edit_data")
+    public void editUserData(@RequestBody User user, String login, String email){
+        user.setLogin(login);
+        user.setEmail(email);
+        userService.updateUser(user);
+    }
+
+    @PutMapping(value = "/edit_pass")
+    public void editUserPass(@RequestBody User user, String password){
+        user.setPassword(password);
+        userService.updateUser(user);
+    }
+}
