150fa915002b3647014997bd4d1c05c415e18ae7
farrukh13git <farrukh_kk@mail.ru>
2019-12-29 21:15:44 +0500

    Изменил сервис downloadmusicvk

--- src/main/java/spring/app/service/impl/DownloadMusicVkRuServiceImpl.java
+++ src/main/java/spring/app/service/impl/DownloadMusicVkRuServiceImpl.java
@@ -11,7 +11,6 @@ import spring.app.service.abstraction.DownloadMusicVkRuService;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
 import java.net.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -57,7 +56,7 @@ public class DownloadMusicVkRuServiceImpl implements DownloadMusicVkRuService {
 
     @Override
     public byte[] getSong(long id) throws IOException {
-        String downloadUrl = temp(id);
+        String downloadUrl = getLinkFromDB(id);
         URL obj = new URL(downloadUrl);
         HttpURLConnection con = (HttpURLConnection) obj.openConnection();
         con.setRequestProperty("User-Agent", "Mozilla/5.0");
@@ -79,66 +78,7 @@ public class DownloadMusicVkRuServiceImpl implements DownloadMusicVkRuService {
         }
     }
 
-    private String temp(long id) {
+    private String getLinkFromDB(long id) {
         return null;
-    }
-
-    public byte[] getSongForTelegram(String artist, String track) {
-        final String SEARCH_BASE_URL = "https://downloadmusicvk.ru/audio/search?q=";
-        if (track.isEmpty()) {
-            return null;
-        }
-        String searchUrl = SEARCH_BASE_URL + artist.trim().toLowerCase().replaceAll("\\s", "+") +
-                "+" + track.trim().toLowerCase().replaceAll("\\s", "+");
-
-        try {
-            Document document = Jsoup.connect(searchUrl).get();
-            if (document == null) {
-                return null;
-            }
-
-            Elements aElements = document.getElementsByAttributeValue("class", "btn btn-primary btn-xs download");
-            for (Element aElement : aElements) {
-                String[] arr = aElement.attr("href").substring(19).split("&");
-                if (arr.length < 6) {
-                    continue;
-                }
-
-                if (!decode(arr[1].substring(7)).replaceAll("\\+", " ").toLowerCase().contains(artist.trim().toLowerCase()) ||
-                        !decode(arr[2].substring(6)).replaceAll("\\+", " ").toLowerCase().contains(track.trim().toLowerCase())) {
-                    continue;
-                }
-                if (artist.isEmpty()) {
-                    artist = decode(arr[1].substring(7)).replaceAll("\\+", " ").toLowerCase();
-                }
-                String downloadUrl = "https://downloadmusicvk.ru/audio/download?" +
-                        arr[1] + "&" + arr[2] + "&" + arr[5] + "&" + arr[0];
-
-                URL obj = new URL(downloadUrl);
-                HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-                con.setRequestProperty("User-Agent", "Mozilla/5.0");
-                int responseCode = con.getResponseCode();
-
-                if (responseCode == HttpURLConnection.HTTP_OK) {
-                    InputStream in = con.getInputStream();
-                    ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-                    int nRead;
-                    byte[] data = new byte[1024];
-                    while ((nRead = in.read(data, 0, data.length)) != -1) {
-                        buffer.write(data, 0, nRead);
-                    }
-                    buffer.flush();
-                    in.close();
-                    return buffer.toByteArray();
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    private String decode(String line) throws UnsupportedEncodingException {
-        return URLDecoder.decode(URLDecoder.decode(line, "UTF-8"), "UTF-8");
     }
 }
--- src/main/java/spring/app/service/impl/TelegramServiceImpl.java
+++ src/main/java/spring/app/service/impl/TelegramServiceImpl.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import spring.app.dto.SongRequest;
 import spring.app.dto.SongResponce;
-import spring.app.service.abstraction.DownloadMusicVkRuService;
 import spring.app.service.abstraction.TelegramService;
 import spring.app.service.abstraction.ZaycevSaitServise;
 
@@ -16,17 +15,15 @@ import java.nio.file.Paths;
 public class TelegramServiceImpl implements TelegramService {
 
     private final ZaycevSaitServise zaycevSaitServise;
-    private final DownloadMusicVkRuServiceImpl downloadMusicVkRuService;
 
-    public TelegramServiceImpl(ZaycevSaitServise zaycevSaitServise, DownloadMusicVkRuServiceImpl downloadMusicVkRuService) {
+    public TelegramServiceImpl(ZaycevSaitServise zaycevSaitServise) {
         this.zaycevSaitServise = zaycevSaitServise;
-        this.downloadMusicVkRuService = downloadMusicVkRuService;
     }
 
     @Override
     public SongResponce getSong(SongRequest songRequest) throws IOException {
 
-        byte[] bytes = downloadMusicVkRuService.getSongForTelegram(songRequest.getAuthorName(),songRequest.getSongName());
+        byte[] bytes = zaycevSaitServise.getSong(songRequest.getAuthorName(),songRequest.getSongName());
         SongResponce songResponce = new SongResponce(songRequest.getChatId(), 242345367l, bytes,
                 songRequest.getAuthorName()+ " - " + songRequest.getSongName());
 
