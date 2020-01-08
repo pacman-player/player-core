9aa247ba12d0c99805a2067087ac92f016435bcb
Taras48 <red.shuhov48@gmail.com>
2020-01-08 14:07:56 +0300

    поправил косяк с GenreDto

--- src/main/java/spring/app/controller/restController/VkBotConversationRestController.java
+++ src/main/java/spring/app/controller/restController/VkBotConversationRestController.java
@@ -1,8 +1,9 @@
 package spring.app.controller.restController;
 
 
+import com.vk.api.sdk.objects.messages.Message;
 import org.json.JSONException;
-import org.json.JSONObject;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,11 +14,21 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping(value = "/api/vk_bot")
 public class VkBotConversationRestController {
 
+    Message message = new Message();
+
     @PostMapping
     public String getVkCallbackAPI(HttpServletRequest request) throws JSONException {
 
-        System.out.println(request.getParameter("type"));
+        System.out.println(request.getParameter("message"));
         System.out.println("connect whis VK complid");
+        return "8e76e4ad";
+    }
+
+    @GetMapping
+    public String getVkMessage(HttpServletRequest request) throws JSONException {
+
+        System.out.println(request.getParameter("type"));
+        System.out.println("asdadsadsadsadasd");
         return "8e76e4ad";
     }
 }
