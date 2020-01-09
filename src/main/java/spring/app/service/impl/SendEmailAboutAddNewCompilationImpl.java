e56b3a701c028aabe69414f976ecae7e82b8b3a3
AlekseyVasilenko89 <53444650+AlekseyVasilenko89@users.noreply.github.com>
2020-01-09 21:19:49 +0300

    Исправил: аннотацию, название, и дао слой

---
+++ src/main/java/spring/app/service/impl/SendEmailAboutAddNewCompilationImpl.java
@@ -0,0 +1,46 @@
+package spring.app.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+import spring.app.model.User;
+import spring.app.service.abstraction.UserService;
+import spring.app.service.abstraction.SendEmailAboutAddNewCompilation;
+
+import java.util.List;
+
+@Service
+public class SendEmailAboutAddNewCompilationImpl implements SendEmailAboutAddNewCompilation {
+
+    private final JavaMailSender emailSender;
+    private final UserService userService;
+
+    @Autowired
+    public SendEmailAboutAddNewCompilationImpl(JavaMailSender emailSender, UserService userService) {
+        this.emailSender = emailSender;
+        this.userService = userService;
+    }
+
+    @Override
+    public void send(String nameCompilation) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        List<User> users = userService.getAllUsers();
+
+        if (users != null) {
+            for (User user : users) {
+                String message = String.format(
+                        "Привет, %s! \n" +
+                                "У нас появилась новая подборка %s",
+                        user.getFirstName(),
+                        nameCompilation
+                );
+                mailMessage.setFrom(user.getLogin());
+                mailMessage.setTo(user.getEmail());
+                mailMessage.setSubject("Новая подборка");
+                mailMessage.setText(message);
+                emailSender.send(mailMessage);
+            }
+        }
+    }
+}
