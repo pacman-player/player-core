240876f18e658510049b95d9f5f13e597e58d672
nick <gnn@mm.st>
2020-01-12 22:53:47 +0300

    merge direct and reverse geocoding

--- src/main/resources/static/js/user/searchYandexMap.js
+++ src/main/resources/static/js/user/searchYandexMap.js
@@ -1,29 +1,29 @@
 $(document).ready(function () {
-// add map
-    ymaps.ready(initReverseGeocoding);
-    function initReverseGeocoding() {
+    ymaps.ready(init);
+    function init() {
+        // Подключаем поисковые подсказки к полю ввода.
+        var suggestView = new ymaps.SuggestView('suggest');
+
         var placemark;
 
+        //Инициализируем карту
         var map = new ymaps.Map('map', {
             center: [55.753902, 37.620074],
             zoom: 12,
             controls: []
         });
 
-
         // Создаем экземпляр класса ymaps.control.SearchControl
-        mySearchControl = new ymaps.control.SearchControl({
+        let mySearchControl = new ymaps.control.SearchControl({
         });
 
         // Создаем экземпляр класса ymaps.control.ZoomControl
-        myZoomControl = new ymaps.control.ZoomControl({
+        let myZoomControl = new ymaps.control.ZoomControl({
         });
 
-        map.controls.add(mySearchControl);
+        //Добавляем в карту зум
+        // map.controls.add(mySearchControl);
         map.controls.add(myZoomControl);
-
-        let alertContent;
-
 
         // Слушаем клик на карте.
         map.events.add('click', function (e) {
@@ -76,7 +76,8 @@ $(document).ready(function () {
 
 
 
-                alertContent = firstGeoObject.getAddressLine();
+
+                let alertContent = firstGeoObject.getAddressLine();
 
                 function putAddressInField () {
                     $('#suggest').val(alertContent);
@@ -89,131 +90,44 @@ $(document).ready(function () {
                 } else {
                     console.log("failed");
                 }
-
-
-
-
             });
         }
-    }
 
-    function initDirectGeocoding() {
-        // Подключаем поисковые подсказки к полю ввода.
-        var suggestView = new ymaps.SuggestView('suggest'),
-            map,
-            placemark;
 
         // При клике по кнопке запускаем верификацию введёных данных.
-        $('#button').bind('click', function (e) {
-            geocode();
-        });
+        $('#button').bind('click', function () {
+            event.preventDefault();
 
-        function geocode() {
             // Забираем запрос из поля ввода.
             var request = $('#suggest').val();
-            // Геокодируем введённые данные.
-            ymaps.geocode(request).then(function (res) {
-                var obj = res.geoObjects.get(0),
-                    error, hint;
 
-                if (obj) {
-                    // Об оценке точности ответа геокодера можно прочитать тут: https://tech.yandex.ru/maps/doc/geocoder/desc/reference/precision-docpage/
-                    switch (obj.properties.get('metaDataProperty.GeocoderMetaData.precision')) {
-                        case 'exact':
-                            break;
-                        case 'number':
-                        case 'near':
-                        case 'range':
-                            error = 'Неточный адрес, требуется уточнение';
-                            hint = 'Уточните номер дома';
-                            break;
-                        case 'street':
-                            error = 'Неполный адрес, требуется уточнение';
-                            hint = 'Уточните номер дома';
-                            break;
-                        case 'other':
-                        default:
-                            error = 'Неточный адрес, требуется уточнение';
-                            hint = 'Уточните адрес';
-                    }
-                } else {
-                    error = 'Адрес не найден';
-                    hint = 'Уточните адрес';
-                }
+            // Поиск координат центра Нижнего Новгорода.
+            ymaps.geocode(request, {
+                results: 1
+            }).then(function (res) {
+                // Выбираем первый результат геокодирования.
+                var firstGeoObject = res.geoObjects.get(0);
 
-                // Если геокодер возвращает пустой массив или неточный результат, то показываем ошибку.
-                if (error) {
-                    showError(error);
-                    showMessage(hint);
-                } else {
-                    showResult(obj);
-                }
-            }, function (e) {
-                console.log(e)
-            })
+                // Координаты геообъекта.
+                var coords = firstGeoObject.geometry.getCoordinates();
 
-        }
-        function showResult(obj) {
-            // Удаляем сообщение об ошибке, если найденный адрес совпадает с поисковым запросом.
-            $('#suggest').removeClass('input_error');
-            $('#notice').css('display', 'none');
+                // Область видимости геообъекта.
+                var bounds = firstGeoObject.properties.get('boundedBy');
 
-            var mapContainer = $('#map'),
-                bounds = obj.properties.get('boundedBy'),
-                // Рассчитываем видимую область для текущего положения пользователя.
-                mapState = ymaps.util.bounds.getCenterAndZoom(
-                    bounds,
-                    [mapContainer.width(), mapContainer.height()]
-                ),
-                // Сохраняем полный адрес для сообщения под картой.
-                address = [obj.getCountry(), obj.getAddressLine()].join(', '),
-                // Сохраняем укороченный адрес для подписи метки.
-                shortAddress = [obj.getThoroughfare(), obj.getPremiseNumber(), obj.getPremise()].join(' ');
-            // Убираем контролы с карты.
-            mapState.controls = [];
-            // Создаём карту.
-            createMap(mapState, shortAddress);
-            // Выводим сообщение под картой.
-            showMessage(address);
-        }
+                firstGeoObject.options.set('preset', 'islands#darkBlueDotIconWithCaption');
 
-        function showError(message) {
-            $('#notice').text(message);
-            $('#suggest').addClass('input_error');
-            $('#notice').css('display', 'block');
-            // Удаляем карту.
-            if (map) {
-                map.destroy();
-                map = null;
-            }
-        }
+                // Получаем строку с адресом и выводим в иконке геообъекта.
+                firstGeoObject.properties.set('iconCaption', firstGeoObject.getAddressLine());
 
-        function createMap(state, caption) {
-            // Если карта еще не была создана, то создадим ее и добавим метку с адресом.
-            if (!map) {
-                map = new ymaps.Map('map', state);
-                placemark = new ymaps.Placemark(
-                    map.getCenter(), {
-                        iconCaption: caption,
-                        balloonContent: caption
-                    }, {
-                        preset: 'islands#redDotIconWithCaption'
-                    });
-                map.geoObjects.add(placemark);
-                // Если карта есть, то выставляем новый центр карты и меняем данные и позицию метки в соответствии с найденным адресом.
-            } else {
-                map.setCenter(state.center, state.zoom);
-                placemark.geometry.setCoordinates(state.center);
-                placemark.properties.set({iconCaption: caption, balloonContent: caption});
-            }
-        }
+                // Добавляем первый найденный геообъект на карту.
+                map.geoObjects.add(firstGeoObject);
 
-        function showMessage(message) {
-            $('#messageHeader').text('Данные получены:');
-            $('#message').text(message);
-        }
+                // Масштабируем карту на область видимости геообъекта.
+                map.setBounds(bounds, {
+                    // Проверяем наличие тайлов на данном масштабе.
+                    checkZoomRange: true
+                });
+            });
+        });
     }
-
-
-
 });
