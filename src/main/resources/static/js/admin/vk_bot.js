cbf5d3c660c04b84d40c3f8cc3a800160b3abe3d
Taras48 <red.shuhov48@gmail.com>
2020-01-14 20:55:20 +0300

    создал сервис VkBotService,он подключается к вк и может отправлятьсообщение в беседу по id беседы создал рест контроллер VkBotRestController так же создана страница и js к ней кто будет проверять, свяжитесь со мной,добавлю в группу в которой работает бот и сделаю админом чтобы можно было создавать беседу

---
+++ src/main/resources/static/js/admin/vk_bot.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+
+
+    $("#sendMessage").click(function (event) {
+        event.preventDefault();
+        sendMessage();
+        $("#idConversation").val("");
+        $("#message").val("");
+    });
+
+    function sendMessage() {
+
+        var conversation = {
+            id: $("#idConversation").val(),
+            message: $("#message").val(),
+
+        };
+
+        $.ajax({
+            type: 'POST',
+            url: "/api/admin/vk_bot/send",
+            contentType: 'application/json;',
+            data: JSON.stringify(conversation),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            async: false,
+            cache: false,
+            complete:
+                function () {
+
+                },
+            success:
+                function () {
+
+                    notification("",
+                        "Отчетотправлен в беседу " + $("#idConversation").val(),
+                        'conversation');
+                },
+            error:
+                function (xhr, status, error) {
+                    alert(xhr.responseText + '|\n' + status + '|\n' + error);
+                }
+        });
+    };
+
+
+});
