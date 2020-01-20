f0458fd39aa5b0f0253de4d938c446711812b993
igor <khilkevichigor@gmail.com>
2020-01-20 22:50:19 +0300

    db-util

---
+++ src/main/java/spring/app/util/HibernateSQLStatementCountTest.java
@@ -0,0 +1,9 @@
+package spring.app.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class HibernateSQLStatementCountTest {
+    @Autowired
+    private DataSource otherDataSource;
+
+}
