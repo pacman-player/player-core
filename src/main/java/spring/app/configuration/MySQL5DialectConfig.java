f680c06680e03a58b63fb144230e72ea629333da
nick <gnn@mm.st>
2020-01-26 11:36:11 +0300

    rename class of Dialect

--- src/main/java/spring/app/configuration/MySQL5DialectConfig.java
+++ src/main/java/spring/app/configuration/MySQL5DialectConfig.java
@@ -1,10 +1,10 @@
-package spring.app.dao;
+package spring.app.configuration;
 
 import org.hibernate.dialect.MySQL5InnoDBDialect;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Dialect extends MySQL5InnoDBDialect {
+public class MySQL5DialectConfig extends MySQL5InnoDBDialect {
     @Override
     public String getTableTypeString() {
         return "ENGINE=InnoDB DEFAULT CHARSET=utf8";
