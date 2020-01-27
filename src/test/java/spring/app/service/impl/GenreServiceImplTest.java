fb27b4dd79f7337f0a6e063ebef3f21f247c034e
AlekseyVasilenko89 <53444650+AlekseyVasilenko89@users.noreply.github.com>
2020-01-27 22:31:22 +0300

    Изменения в проперти

--- src/test/java/spring/app/service/impl/GenreServiceImplTest.java
+++ src/test/java/spring/app/service/impl/GenreServiceImplTest.java
@@ -1,6 +1,8 @@
 package spring.app.service.impl;
 
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
