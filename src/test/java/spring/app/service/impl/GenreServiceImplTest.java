f052cb294cb9cfb13090c5e0592e7f6a3bd37cd3
AlekseyVasilenko89 <53444650+AlekseyVasilenko89@users.noreply.github.com>
2020-01-27 22:33:42 +0300

    Изменения в проперти

--- src/test/java/spring/app/service/impl/GenreServiceImplTest.java
+++ src/test/java/spring/app/service/impl/GenreServiceImplTest.java
@@ -1,10 +1,21 @@
 package spring.app.service.impl;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import spring.app.service.abstraction.GenreService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class GenreServiceImplTest {
+
+    @Autowired
+    private GenreService genreService;
+
+    @Test
+    public void contexLoads() throws Exception {
+        assertThat(genreService).isNotNull();
+    }
 }
