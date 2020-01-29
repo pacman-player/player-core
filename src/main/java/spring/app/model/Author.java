3004b5f18a651d7ce039b072e844530e20624d00
igor <khilkevichigor@gmail.com>
2020-01-29 23:27:18 +0300

    добавил аннотацию в Author для сериализации объекта при LAZY

--- src/main/java/spring/app/model/Author.java
+++ src/main/java/spring/app/model/Author.java
@@ -1,6 +1,5 @@
 package spring.app.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import javax.persistence.*;
@@ -9,6 +8,7 @@ import java.util.Set;
 
 @Entity
 @Table(name = "author")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"}) //объекты загружаются лениво, и сериализация происходит до того как они будут загружены полность. Без этой аннотаци не отображаются песни на странице в админке
 public class Author extends Bannable{
 
     @Id
