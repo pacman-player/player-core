c56950cc32eae3394141e29bb0067ce9fc2ac12a
igor <khilkevichigor@gmail.com>
2020-01-30 14:02:44 +0300

    добавил UserValidator and validation.properties

---
+++ src/main/java/spring/app/util/UserValidator.java
@@ -0,0 +1,17 @@
+package spring.app.util;
+
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+public class UserValidator implements Validator {
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return false;
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+
+    }
+}
