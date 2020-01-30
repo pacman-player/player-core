2a44723a306aa8dc84b0c0ce795fa6d4ab502da9
igor <khilkevichigor@gmail.com>
2020-01-30 18:20:45 +0300

    добавил проверку через биндингРесалт

--- src/main/java/spring/app/controller/controller/LoginController.java
+++ src/main/java/spring/app/controller/controller/LoginController.java
@@ -1,13 +1,15 @@
 package spring.app.controller.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import spring.app.dto.CaptchaResponseDto;
+import spring.app.model.User;
+import spring.app.util.UserValidator;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -23,16 +25,29 @@ public class LoginController {
     private String secret;
     private RestTemplate restTemplate = new RestTemplate();
 
+    @Autowired
+    private UserValidator userValidator;
+
+//    @GetMapping("/login")
+//    public String getLogin(Model model, String error) {
+//        if (error != null) {
+//            model.addAttribute("error", "Your username and password is invalid.");
+//        }
+//        return "login";
+//    }
+
     @PostMapping("/login")
-    public void login(
-            HttpServletResponse response,
-            HttpServletRequest request
-    ) throws IOException, ServletException {
+    public void login(HttpServletResponse response, HttpServletRequest request, @ModelAttribute("userForm") User userForm, BindingResult bindingResult) throws IOException, ServletException {
+        userValidator.validate(userForm, bindingResult);
+
+        if (bindingResult.hasErrors()) {
+            response.sendRedirect("/login");
+        }
+
         int loginAttempt = 1;
         HttpSession session = request.getSession();
 
-        if (session.getAttribute("loginCount") == null)
-        {
+        if (session.getAttribute("loginCount") == null) {
             session.setAttribute("loginCount", 1);
         }
 
--- src/main/java/spring/app/util/UserValidator.java
+++ src/main/java/spring/app/util/UserValidator.java
@@ -1,10 +1,20 @@
 package spring.app.util;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
+import spring.app.model.User;
+import spring.app.service.abstraction.UserService;
 
+@Component
 public class UserValidator implements Validator {
+
+    @Autowired
+    private UserService userService;
+
     @Override
     public boolean supports(Class<?> aClass) {
         return false;
@@ -12,6 +22,22 @@ public class UserValidator implements Validator {
 
     @Override
     public void validate(Object o, Errors errors) {
+        User user = (User) o;
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "username", "NotEmpty");
+        if (user.getUsername().length() < 6 || user.getUsername().length() > 32) {
+            errors.rejectValue("username", "Size.userForm.username");
+        }
+        if (userService.getUserByLogin(user.getUsername()) != null) {
+            errors.rejectValue("username", "Duplicate.userForm.username");
+        }
 
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "NotEmpty");
+        if (user.getPassword().length() < 8 || user.getPassword().length() > 32) {
+            errors.rejectValue("password", "Size.userForm.password");
+        }
+
+//        if (!user.getPasswordConfirm().equals(user.getPassword())) {
+//            errors.rejectValue("passwordConfirm", "Diff.userForm.passwordConfirm");
+//        }
     }
 }
