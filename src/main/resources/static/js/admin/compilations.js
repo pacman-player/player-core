687ca1ea8079e289948f28ca378b843b8b667c29
fatumepta <fatumepta-git@yandex.ru>
2020-02-20 23:38:10 +0300

    UPD: compilations.js + -> добавил функцию отрисовки таблицы

--- src/main/resources/static/js/admin/compilations.js
+++ src/main/resources/static/js/admin/compilations.js
@@ -1,48 +1,53 @@
-// $(document).ready(function () {
-//     getTable();
-// });
-//
-// function getTable() {
-//     $.ajax({
-//         method: "GET",
-//         url: "/api/admin/compilation",
-//         contentType: "application/json",
-//         headers: {
-//             "Accept": "application/json",
-//             "Content-Type": "application/json"
-//         },
-//         dataType: "JSON",
-//         success: function (compilations) {
-//             let tableBody = $("#genresTable tbody");
-//
-//             tableBody.empty();
-//             for (let i = 0; i < genres.length; i++) {
-//                 // vars that contains object's fields
-//                 let id = genres[i].id;
-//                 let name = genres[i].name;
-//                 // parsing fields
-//                 let tr = $("<tr/>");
-//                 tr.append(`
-//                             <td> ${id} </td>
-//                             <td> ${name} </td>
-//                             <td>
-//                                 <button type="submit"
-//                                         class="btn btn-sm btn-info"
-//                                         id="editGenreBtn"
-//                                         onclick="editButton(${id}, '${name}')">
-//                                     Изменить
-//                                 </button>
-//                             </td>
-//                             <td>
-//                                 <button type="button"
-//                                         class="btn btn-sm btn-info"
-//                                         id="deleteGenreBtn"
-//                                         onclick="deleteButton(${id})">
-//                                     Удалить
-//                                 </button>
-//                             </td>`);
-//                 tableBody.append(tr);
-//             }
-//         }
-//     });
-// }
+$(document).ready(function () {
+    getTable();
+});
+
+function getTable() {
+    $.ajax({
+        method: "GET",
+        url: "/api/admin/compilation",
+        contentType: "application/json",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        dataType: "JSON",
+        success: function (compilations) {
+            let tableBody = $("#compilationsTable tbody");
+
+            tableBody.empty();
+            for (let i = 0; i < compilations.length; i++) {
+                let id = compilations[i].id;
+                let name = compilations[i].name;
+                let genre = compilations[i].genre.name;
+                let songs;
+                let cover;
+
+                let tr = $("<tr/>");
+                tr.append(`
+                            <td> ${id} </td>
+                            <td> ${name} </td>
+                            <td> ${genre} </td>
+                            <td> songs </td>
+                            <td> cover </td>
+                            <td>
+                                <button type="submit"
+                                        class="btn btn-sm btn-info"
+                                        id="editGenreBtn"
+                                        onclick="editButton(${id}, '${name}')">
+                                    Изменить
+                                </button>
+                            </td>
+                            <td>
+                                <button type="button"
+                                        class="btn btn-sm btn-info"
+                                        id="deleteGenreBtn"
+                                        onclick="deleteButton(${id})">
+                                    Удалить
+                                </button>
+                            </td>`);
+                tableBody.append(tr);
+            }
+        }
+    });
+}
