91e9f744ff05c7ee194c6962341b097aa8472705
igor <khilkevichigor@gmail.com>
2020-02-24 13:42:45 +0300

    сделал navbar

--- src/main/resources/static/spa/views/components/Bottombar.js
+++ src/main/resources/static/spa/views/components/Bottombar.js
@@ -1,14 +1,16 @@
 let Bottombar = {
     render: async () => {
-        let view =  /*html*/`
-        <footer class="footer">
-            <div class="content has-text-centered">
-                <p>
-                    This is my foot. There are many like it, but this one is mine.
-                </p>
-            </div>
-        </footer>
-        `
+        let view =  /*html*/
+            `
+            <footer class="footer">
+                <div class="content has-text-centered">
+                    <p>
+                        Здесь можно воткнуть плеер
+                    </p>
+                </div>
+            </footer>
+            `
+
         return view
     },
     after_render: async () => { }
--- src/main/resources/static/spa/views/components/Navbar.js
+++ src/main/resources/static/spa/views/components/Navbar.js
@@ -1,48 +1,61 @@
 let Navbar = {
     render: async () => {
-        let view =  /*html*/`
-             <nav class="navbar" role="navigation" aria-label="main navigation">
-                <div class="container">
-                    <div class="navbar-brand">
-                        <a class="navbar-item" href="/#/">
-                            <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
-                        </a>
+        let view =  /*html*/
+        //     `
+        //      <nav class="navbar" role="navigation" aria-label="main navigation">
+        //         <div class="container">
+        //             <div class="navbar-brand">
+        //                 <a class="navbar-item" href="/#/">
+        //                     <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+        //                 </a>
+        //
+        //                 <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+        //                     <span aria-hidden="true"></span>
+        //                     <span aria-hidden="true"></span>
+        //                     <span aria-hidden="true"></span>
+        //                 </a>
+        //             </div>
+        //
+        //             <div id="navbarBasicExample" class="navbar-menu is-active" aria-expanded="false">
+        //                 <div class="navbar-start">
+        //                     <a class="navbar-item" href="/#/">
+        //                         Home
+        //                     </a>
+        //                     <a class="navbar-item" href="/#/about">
+        //                         About
+        //                     </a>
+        //                     <a class="navbar-item" href="/#/secret">
+        //                         Secret
+        //                     </a>
+        //                 </div>
+        //                 <div class="navbar-end">
+        //                     <div class="navbar-item">
+        //                         <div class="buttons">
+        //                             <a class="button is-primary" href="/#/register">
+        //                                 <strong>Sign up</strong>
+        //                             </a>
+        //                             <a class="button is-light">
+        //                                 Log in
+        //                             </a>
+        //                         </div>
+        //                     </div>
+        //                 </div>
+        //             </div>
+        //         </div>
+        //     </nav>
+        // `
 
-                        <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-                            <span aria-hidden="true"></span>
-                            <span aria-hidden="true"></span>
-                            <span aria-hidden="true"></span>
-                        </a>
-                    </div>
+            '<div class="container-fluid">\n' +
+            '    <div class="row">\n' +
+            '        <div class="navbar navbar-inverse navbar-static-top header">\n' +
+            '            <a class="navbar-brand" href="#"><b>Pacman player</b></a>\n' +
+            '            <a class="navbar-brand pull-right" href="/logout">Выход</a>\n' +
+            '            <button id="notification" class="navbar-brand pull-right">Уведомлений</button>\n' +
+            '            <div id="listNotification" class="pull-right"></div>\n' +
+            '        </div>\n' +
+            '    </div>\n' +
+            '</div>'
 
-                    <div id="navbarBasicExample" class="navbar-menu is-active" aria-expanded="false">
-                        <div class="navbar-start">
-                            <a class="navbar-item" href="/#/">
-                                Home
-                            </a>
-                            <a class="navbar-item" href="/#/about">
-                                About
-                            </a>
-                            <a class="navbar-item" href="/#/secret">
-                                Secret
-                            </a>
-                        </div>
-                        <div class="navbar-end">
-                            <div class="navbar-item">
-                                <div class="buttons">
-                                    <a class="button is-primary" href="/#/register">
-                                        <strong>Sign up</strong>
-                                    </a>
-                                    <a class="button is-light">
-                                        Log in
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </nav>
-        `
         return view
     },
     after_render: async () => { }
