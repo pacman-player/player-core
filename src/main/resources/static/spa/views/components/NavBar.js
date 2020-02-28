19f634a8db10ce38a760e55474d62268dc968778
igor <khilkevichigor@gmail.com>
2020-02-28 13:19:05 +0300

    верстка.

--- src/main/resources/static/spa/views/components/NavBar.js
+++ src/main/resources/static/spa/views/components/NavBar.js
@@ -45,7 +45,6 @@ let NavBar = {
         //     </nav>
         // `
 
-            '<div class="container-fluid">\n' +
             '    <div class="row">\n' +
             '        <div class="navbar navbar-inverse navbar-static-top header">\n' +
             '            <a class="navbar-brand" href="#"><b>Pacman player</b></a>\n' +
@@ -53,8 +52,7 @@ let NavBar = {
             '            <button id="notification" class="navbar-brand pull-right">Уведомлений</button>\n' +
             '            <div id="listNotification" class="pull-right"></div>\n' +
             '        </div>\n' +
-            '    </div>\n' +
-            '</div>'
+            '    </div>\n'
 
         return view
     },
