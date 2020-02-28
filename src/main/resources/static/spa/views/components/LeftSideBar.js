a056116121d8c36dd2c69965948233b731ce31e7
igor <khilkevichigor@gmail.com>
2020-02-28 23:13:13 +0300

    админская кнопка, подключил яндекс.карту, формы заполняются

--- src/main/resources/static/spa/views/components/LeftSideBar.js
+++ src/main/resources/static/spa/views/components/LeftSideBar.js
@@ -30,7 +30,28 @@ let LeftSideBar = {
 
         return view
     },
-    after_render: async () => { }
+    after_render: async () => {
+
+        $(document).ready(function () {
+
+            //доступ к  ссылки админа
+            showLinkAdmin();
+
+            function showLinkAdmin() {
+                $.ajax({
+                    type: "post",
+                    url: "/api/user/show_admin",
+                    success: function (role) {
+                        if (role !== "admin") {
+                            $("#adminLink").hide();
+                        }
+                    }
+                });
+            }
+
+
+        })
+    }
 
 }
 
