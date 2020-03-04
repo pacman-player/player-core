697c0b0d9034298db53d905a83f93d5a08c439d6
Anastasia <anastasia0985@mail.ru>
2020-03-04 16:50:27 +0300

    - правки удаления юзеров и компаний. смержено с dev.

--- src/main/java/spring/app/dao/impl/OrderSongDaoImpl.java
+++ src/main/java/spring/app/dao/impl/OrderSongDaoImpl.java
@@ -44,21 +44,6 @@ public class OrderSongDaoImpl extends AbstractDao<Long, OrderSong> implements Or
                 .setParameter("id", id).getSingleResult();
     }
 
-    /*Select ...
-From Keywords As K
-    Left Join NegativeKeywords As NK
-        On NK.EntityKey = K.EntityKey
-    Left Join RequiredKeywords As RK
-        On RK.EntityKey = K.EntityKey
-Where NK.EntityKey Is Null
-    And (
-        Not Exists  (
-                    Select 1
-                    From RequiredKeywords As RK1
-                    Where RK1.EntityKey = K.EntityKey
-                    )
-        Or RK.EntityKey Is Not Null
-        )*/
     @Override
     public List<Long> getAllByCompanyId(Long companyId) {
 
@@ -68,23 +53,13 @@ Where NK.EntityKey Is Null
                         "WHERE osng.company_id = ?"
         );
         q.setParameter(1, companyId);
-        List stepsList = q.getResultList();
+        List idsList = q.getResultList();
         List<Long> resultList = new ArrayList<>();
-        for (Object num: stepsList
+        for (Object num : idsList
         ) {
-            resultList.add(((BigInteger)num).longValue());
+            resultList.add(((BigInteger) num).longValue());
         }
         return resultList;
-
-        /*return entityManager.createQuery(
-                "SELECT osng.id " +
-                        "FROM OrderSong AS osng " +
-                        "LEFT JOIN Company AS cmp " +
-                        "ON osng.company = cmp " +
-                        "WHERE cmp.id = :companyId",
-                Long.class)
-                .setParameter("companyId", companyId)
-                .getResultList();*/
     }
 
     @Override
