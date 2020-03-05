3f03b3435fa4849084c7bb94e4bad21b36075dd5
alekseeyn <alekseynesmahn@gmail.com>
2020-03-05 16:49:39 +0400

    добавлены два метода возвращающие листы с Song_id

--- src/main/java/spring/app/dao/abstraction/OrderSongDao.java
+++ src/main/java/spring/app/dao/abstraction/OrderSongDao.java
@@ -3,10 +3,14 @@ package spring.app.dao.abstraction;
 import spring.app.model.OrderSong;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 public interface OrderSongDao extends GenericDao<Long, OrderSong> {
 
     long getSongOrdersByCompanyIdAndPeriod(Long id, Timestamp after);
     long getSongOrdersByCompanyIdAndTimeRange(Long id, Timestamp start, Timestamp end);
     long countAll(Long id);
+
+    List<Long> getListSongIdByCompanyIdAndPeriod(Long id, Timestamp after);
+    List<Long> getListSongIdByCompanyIdAndTimeRange(Long id, Timestamp start, Timestamp end);
 }
