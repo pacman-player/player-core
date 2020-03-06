4513f3c817464edbe2d006ecdaf9058d42353bb8
alekseeyn <alekseynesmahn@gmail.com>
2020-03-06 14:41:43 +0400

    добавлены методы возвращающие листы с song_id

--- src/main/java/spring/app/service/impl/OrderSongServiceImpl.java
+++ src/main/java/spring/app/service/impl/OrderSongServiceImpl.java
@@ -10,6 +10,7 @@ import spring.app.service.abstraction.OrderSongService;
 import java.sql.Timestamp;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 @Service
 @Transactional
@@ -59,4 +60,32 @@ public class OrderSongServiceImpl implements OrderSongService {
     public long countAll(Long companyId) {
         return orderSongDao.countAll(companyId);
     }
+
+
+    @Override
+    public List<Long> getListSongIdByCompanyIdAndPeriod(Long id, Long period) {
+        return orderSongDao.getListSongIdByCompanyIdAndPeriod(id, new Timestamp(System.currentTimeMillis() - period * 24 * 60 * 60 * 1000));
+    }
+
+    @Override
+    public List<Long> getListSongIdByCompanyIdAndTimeRange(Long id, String period) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.HOUR_OF_DAY, 0);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.SECOND, 0);
+        Date today = cal.getTime();
+        Timestamp startOfTheDayToday = new Timestamp(today.getTime());
+        cal.add(Calendar.DATE, -1);
+        cal.set(Calendar.HOUR_OF_DAY, 0);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.SECOND, 0);
+        Date yesterday = cal.getTime();
+        Timestamp startOfTheDayYesterday = new Timestamp(yesterday.getTime());
+        List<Long> result = orderSongDao.getListSongIdByCompanyIdAndTimeRange(id, startOfTheDayToday, new Timestamp(System.currentTimeMillis()));
+        if (period.equals("yesterday")) {
+            result = orderSongDao.getListSongIdByCompanyIdAndTimeRange(id, startOfTheDayYesterday, startOfTheDayToday);
+        }
+        return result;
+    }
 }
+
