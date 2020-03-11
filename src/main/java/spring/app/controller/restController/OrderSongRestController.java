f863f2cfe334060903a8e2cae33151f7e1e1a6de
alekseeyn <alekseynesmahn@gmail.com>
2020-03-11 05:11:45 +0400

    добавлен обработчик возвращающий топ песен

--- src/main/java/spring/app/controller/restController/OrderSongRestController.java
+++ src/main/java/spring/app/controller/restController/OrderSongRestController.java
@@ -5,15 +5,18 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import spring.app.model.Song;
 import spring.app.model.User;
 import spring.app.service.abstraction.CompanyService;
 import spring.app.service.abstraction.OrderSongService;
+import spring.app.service.abstraction.SongService;
 import spring.app.service.abstraction.UserService;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/api/v1")
@@ -22,7 +25,7 @@ public class OrderSongRestController {
     private OrderSongService orderSongService;
 
     private UserService userService;
-
+    private SongService songService;
     private CompanyService companyService;
 
     @Autowired
@@ -32,6 +35,10 @@ public class OrderSongRestController {
     @Autowired
     public void setUserService(UserService userService) {
         this.userService = userService;
+    }
+    @Autowired
+    public void setSongService(SongService songService) {
+        this.songService = songService;
     }
     @Autowired
     public void setCompanyService(CompanyService companyService) {
@@ -63,5 +70,44 @@ public class OrderSongRestController {
         List<String> queueList = companyService.getAllSongsInQueueByCompanyId(companyId);
         LOGGER.info("Result has {} lines", queueList.size());
         return queueList;
+    }
+
+
+    @GetMapping(value = "/getTopSong/{period}")
+    public Map<String, Long> getTopSongPeriod(@PathVariable Long period) {
+        User user = (User)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        user = userService.getUserByLogin(user.getUsername());
+        Long companyId = user.getCompany().getId();
+        List<Long> listSongId = orderSongService.getListSongIdByCompanyIdAndPeriod(companyId, period);
+        Map<Long, Long> songIdAndNumbersRepeat = new HashMap<>();
+        for (Long songId : listSongId) {
+            if (!songIdAndNumbersRepeat.containsKey(songId)) {
+                songIdAndNumbersRepeat.put(songId, 1L);
+            } else {
+                songIdAndNumbersRepeat.put(songId, songIdAndNumbersRepeat.get(songId) + 1);
+            }
+        }
+
+
+             Map<Long, Long> resultLong = songIdAndNumbersRepeat.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                     .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+                             (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+
+        Set<Long> setSongId = resultLong.keySet();
+        Map<String, Long> result = new LinkedHashMap<>();
+        for(Long songId : setSongId) {
+            result.put(songService.getSongById(songId).getAuthor().getName() + ", " +
+                    songService.getSongById(songId).getName(), resultLong.get(songId));
+        }
+        return result;
+    }
+
+    @GetMapping(value = "/getTopSongTimeRange")
+    public List<Long> getTopSongTimeRange() {
+        User user = (User)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        user = userService.getUserByLogin(user.getUsername());
+        Long companyId = user.getCompany().getId();
+        return orderSongService.getListSongIdByCompanyIdAndTimeRange(companyId, "today");
     }
 }
