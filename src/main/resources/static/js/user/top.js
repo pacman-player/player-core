2f7aad241d81bd93b046a46afa1580da2748f031
alekseeyn <alekseynesmahn@gmail.com>
2020-03-11 05:08:55 +0400

    доавлены методы генерации таблиц

--- src/main/resources/static/js/user/top.js
+++ src/main/resources/static/js/user/top.js
@@ -2,6 +2,90 @@ $(document).ready(function () {
 
     //доступ к  ссылки админа
     showLinkAdmin();
+    topDaySong();
+    topWeekSong();
+    topMonthSong();
+    topYearSong();
+
+    function topDaySong() {
+        $.ajax({
+            url : '/api/v1/getTopSong/1',
+            dataType: "json",
+            contentType: "application/json;charset=utf-8",
+            success : function(data) {
+                let topId = 0;
+                $.each(data , function( key, value) {
+                    topId++;
+                    $('<tr>').html(
+                        "<td>" + topId + "</td><td>"
+                        + key + "</td><td>"
+                        + value + "</td>")
+                        .appendTo('#myTableDay');
+                });
+
+            }
+        });
+    }
+
+    function topWeekSong() {
+        $.ajax({
+            url : '/api/v1/getTopSong/7',
+            dataType: "json",
+            contentType: "application/json;charset=utf-8",
+            success : function(data) {
+                let topId = 0;
+                $.each(data , function( key, value ) {
+                    topId++;
+                    $('<tr>').html(
+                        "<td>" + topId + "</td><td>"
+                        + key + "</td><td>"
+                        + value + "</td>")
+                        .appendTo('#myTableWeek');
+                });
+
+            }
+        });
+    }
+
+    function topMonthSong() {
+        $.ajax({
+            url : '/api/v1/getTopSong/30',
+            dataType: "json",
+            contentType: "application/json;charset=utf-8",
+            success : function(data) {
+                let topId = 0;
+                $.each(data , function( key, value ) {
+                    topId++;
+                    $('<tr>').html(
+                        "<td>" + topId + "</td><td>"
+                        + key + "</td><td>"
+                        + value + "</td>")
+                        .appendTo('#myTableMonth');
+                });
+
+            }
+        });
+    }
+
+    function topYearSong() {
+        $.ajax({
+            url : '/api/v1/getTopSong/365',
+            dataType: "json",
+            contentType: "application/json;charset=utf-8",
+            success : function(data) {
+                let topId = 0;
+                $.each(data , function( key, value ) {
+                    topId++;
+                    $('<tr>').html(
+                        "<td>" + topId + "</td><td>"
+                        + key + "</td><td>"
+                        + value + "</td>")
+                        .appendTo('#myTableYear');
+                });
+
+            }
+        });
+    }
 
     function showLinkAdmin() {
         $.ajax({
