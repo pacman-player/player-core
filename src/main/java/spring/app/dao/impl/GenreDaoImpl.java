a150b33f00563f9a764c8fcbd7beff09234c1474
YuraForward <iury.fedoroff2018@yandex.ru>
2020-04-22 08:39:02 +0300

    все работает

--- src/main/java/spring/app/dao/impl/GenreDaoImpl.java
+++ src/main/java/spring/app/dao/impl/GenreDaoImpl.java
@@ -10,7 +10,6 @@ import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import java.sql.Timestamp;
 import java.util.List;
-
 @Repository
 @Transactional(readOnly = true)
 public class GenreDaoImpl extends AbstractDao<Long, Genre> implements GenreDao {
@@ -18,31 +17,16 @@ public class GenreDaoImpl extends AbstractDao<Long, Genre> implements GenreDao {
         super(Genre.class);
     }
 
+
     @Override
     public void deleteById(Long id) {
-        TypedQuery<SongCompilation> query = (TypedQuery<SongCompilation>) entityManager.createQuery("UPDATE SongCompilation SET genre_id = null WHERE genre_id = :id");
-        query.setParameter("id", id);
+        Query query = entityManager.createQuery("DELETE FROM Genre WHERE id = " + id);
         query.executeUpdate();
-
-        TypedQuery<Company> queryCompany = (TypedQuery<Company>) entityManager.createNativeQuery("DELETE FROM company_on_banned_genre WHERE genre_id = :id");
-        queryCompany.setParameter("id", id);
-        queryCompany.executeUpdate();
-
-        TypedQuery<OrgType> queryOrgType = (TypedQuery<OrgType>) entityManager.createNativeQuery("DELETE FROM org_type_on_related_genre WHERE genre_id = :id");
-        queryOrgType.setParameter("id", id);
-        queryOrgType.executeUpdate();
-
-        TypedQuery<Author> queryAuthor = (TypedQuery<Author>) entityManager.createNativeQuery("DELETE FROM author_on_genre WHERE genre_id = :id");
-        queryAuthor.setParameter("id", id);
-        queryAuthor.executeUpdate();
-
-        TypedQuery<Song> querySong = (TypedQuery<Song>) entityManager.createQuery("UPDATE Song SET genre_id = null WHERE genre_id = :id");
-        querySong.setParameter("id", id);
-        querySong.executeUpdate();
-
-        super.deleteById(id);
     }
 
+
+
+    //  @Transactional(readOnly = true)
     @Override
     public Genre getByName(String name) {
         TypedQuery<Genre> query = entityManager.createQuery("SELECT u FROM Genre u WHERE u.name = :name", Genre.class);
@@ -56,19 +40,50 @@ public class GenreDaoImpl extends AbstractDao<Long, Genre> implements GenreDao {
         return genre;
     }
 
+
     @Override
     public List<Genre> getByCreatedDateRange(Timestamp dateFrom, Timestamp dateTo) {
         return entityManager
                 .createQuery("FROM Genre g WHERE g.createdAt >= :dateFrom AND g.createdAt <= :dateTo ORDER BY g.createdAt", Genre.class)
                 .setParameter("dateFrom", dateFrom)
-                .setParameter("dateTo", dateTo).getResultList();
+                .setParameter("dateTo", dateTo)
+                .getResultList();
     }
 
+    //  @Transactional(readOnly = true)
     public List<Genre> getAllApproved() {
         String genericClassName = Genre.class.toGenericString();
         genericClassName = genericClassName.substring(genericClassName.lastIndexOf('.') + 1);
         String hql = "FROM " + genericClassName + " as c WHERE c.isApproved = true";
         TypedQuery<Genre> query = entityManager.createQuery(hql, Genre.class);
         return query.getResultList();
+    }
+
+    @Override
+    public List<Song> getSongsByGenre(Genre genre) {
+        TypedQuery<Song> query = entityManager.createQuery("SELECT u FROM Song u WHERE u.genre = :genre", Song.class);
+        query.setParameter("genre", genre);
+        List<Song> songs=query.getResultList();
+        return songs;
+    }
+
+    @Override
+    public void deleteReferenceFromOrgTypeByGenre(Genre genre) {
+        Long id=genre.getId();
+        TypedQuery<OrgType> queryOrgType = (TypedQuery<OrgType>) entityManager.createNativeQuery("DELETE FROM org_type_on_related_genre WHERE genre_id = :id");
+        queryOrgType.setParameter("id", id);
+        queryOrgType.executeUpdate();
+    }
+    @Override
+    public void deleteReferenceFromCompanyByGenre(Genre genre) {
+        TypedQuery<Company> queryOrgType = (TypedQuery<Company>) entityManager.createNativeQuery("DELETE FROM company_on_banned_genre WHERE genre_id ="+genre.getId());
+        queryOrgType.executeUpdate();
+    }
+    public boolean contains(SongCompilation songCompilation) {
+        return entityManager.contains(songCompilation);
+    }
+    @Override
+    public void flush() {
+        entityManager.flush();
     }
 }
--- src/main/java/spring/app/service/impl/GenreServiceImpl.java
+++ src/main/java/spring/app/service/impl/GenreServiceImpl.java
@@ -4,21 +4,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import spring.app.dao.abstraction.GenreDao;
+import spring.app.dao.abstraction.SongDao;
+import spring.app.dao.impl.GenreDaoImpl;
+import spring.app.model.Author;
 import spring.app.model.Genre;
+import spring.app.model.Song;
+import spring.app.model.SongCompilation;
 import spring.app.service.abstraction.GenreService;
+import spring.app.service.abstraction.SongCompilationService;
 
 import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.List;
+
 
 @Service
 @Transactional
 public class GenreServiceImpl implements GenreService {
-
     private final GenreDao genreDao;
+    private final SongDao songDao;
+    private final SongCompilationService songCompilationService;
+
 
     @Autowired
-    public GenreServiceImpl(GenreDao genreDao) {
+    public GenreServiceImpl(GenreDao genreDao, SongDao songDao, SongCompilationService songCompilationService) {
         this.genreDao = genreDao;
+        this.songDao = songDao;
+        this.songCompilationService = songCompilationService;
     }
 
     @Override
@@ -33,6 +45,30 @@ public class GenreServiceImpl implements GenreService {
 
     @Override
     public void deleteGenreById(Long id) {
+        Genre notDefinedGenre = getByName("not defined");
+        Genre genreForDelete = getById(id);
+        // в сервисе только таким образом можно пробежать по всем авторам , удалить у них наш жанр и в случае если жанров у них больше нет добавить "не определенный"
+        List<Author> authors = new ArrayList<>(genreForDelete.getAuthors());
+        for (Author author : authors) {
+            author.getAuthorGenres().remove(genreForDelete);
+            if (author.getAuthorGenres().size() == 0) {
+                author.getAuthorGenres().add(notDefinedGenre);
+            }
+        }
+
+        List<Song> songs = genreDao.getSongsByGenre(genreForDelete);
+        for (Song song : songs) {
+            song.setGenre(notDefinedGenre);
+        }
+        genreDao.deleteReferenceFromOrgTypeByGenre(genreForDelete);
+        genreDao.deleteReferenceFromCompanyByGenre(genreForDelete);
+
+        List<SongCompilation> songCompilationList = new ArrayList<>(genreForDelete.getSongCompilation());
+        for (SongCompilation songCompilation : songCompilationList) {
+            songCompilation.setGenre(notDefinedGenre);
+            genreForDelete.getSongCompilation().remove(songCompilation);
+        }
+        genreDao.flush();
         genreDao.deleteById(id);
     }
 
