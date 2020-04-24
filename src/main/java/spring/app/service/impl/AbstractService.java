4701fa8a2a36d0e214229bc250ef74b0f562c681
NikolayChehow <57003198+NikolayChehow@users.noreply.github.com>
2020-04-25 02:43:29 +0300

    удалил задвоение методов с интерфейса

--- src/main/java/spring/app/service/impl/AbstractService.java
+++ src/main/java/spring/app/service/impl/AbstractService.java
@@ -1,32 +1,19 @@
 package spring.app.service.impl;
 
 import spring.app.dao.abstraction.GenericDao;
+import spring.app.service.abstraction.GenericService;
 
 import java.io.Serializable;
 import java.util.List;
 
 
-public abstract class AbstractService<PK extends Serializable, T, R extends GenericDao<PK, T>>  {
+public abstract class AbstractService<PK extends Serializable, T, R extends GenericDao<PK, T>> implements GenericService<PK, T> {
 
     final protected R dao;
 
     protected AbstractService(R dao) {
         this.dao = dao;
     }
-
-    public abstract void save(T entity);
-
-    public abstract T getById(PK id);
-
-    public abstract void update(T entity);
-
-    public abstract void deleteById(PK id);
-
-    public abstract boolean isExistById(PK id);
-
-    public abstract List<T> findByNameContaining(String param);
-
-    public abstract List<T> getAll();
 
 }
 
