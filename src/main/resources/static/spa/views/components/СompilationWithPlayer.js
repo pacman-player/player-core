fc8a10354d763fd69c556742741fd570d73803f6
Alexander-Bolotov <37707228+Alexander-Bolotov@users.noreply.github.com>
2020-04-29 19:33:24 +0400

    Исправил очистку плейлиста от уже проигранных заказанных песен

--- src/main/resources/static/spa/views/components/СompilationWithPlayer.js
+++ src/main/resources/static/spa/views/components/СompilationWithPlayer.js
@@ -1042,11 +1042,14 @@ function playNext() {
             } else {
 
                 //очищаем список песен текущего плейлиста от заказанных песен
+                let playListFree = []; // в этот массив добавляются песни isFormSongQueue = false, т.е. которые не были в очереди заказов
                 for (let i = 0; i < allSongsInCurrentPlaylist.length; i++) {
-                    if (allSongsInCurrentPlaylist[i].isFromSongQueue) {
-                        allSongsInCurrentPlaylist.splice(i, 1)
+                    if (!allSongsInCurrentPlaylist[i].isFromSongQueue) {
+                        playListFree.push(allSongsInCurrentPlaylist[i]);
                     }
                 }
+                allSongsInCurrentPlaylist =[];
+                allSongsInCurrentPlaylist = playListFree;
 
                 //следующий плейлист является последним воспроизведенным плейлистом
                 let nextPlaylistName = lastPlayedPlaylistName;
